2b71faeb3497880dffdd362745022de697e9d471
skngetich <stephenngetich@gmail.com>
2020-05-14 11:23:51 +0300

    added admin and accident model schema.

---
+++ functions/models/accident.model.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const uuid = require('uuid/v4');
+
+const schema = mongoose.Schema; 
+
+
+const accidentSchema = new schema(
+  {
+    case_id: {
+      type: String,
+      default: uuid(),
+      unique: true
+    },
+    no_of_victims: {
+      type: String,
+      required: true,
+      unique: false
+    },
+    deviceType: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    reportedTime: {
+      type: Date,
+      required: true
+    },
+    location: {
+      submitted_location:{
+        type:String,
+        required:true
+      },
+      latitude:{
+        type: Number,
+        required:false
+      },
+      longitude:{
+        type:Number,
+        required:false
+      }
+    },
+    weather_type: {
+      type: String,
+      required: false
+    }
+  },
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt'
+    }
+  }
+);
+
+
+accidentSchema.plugin(uniqueValidator);
+const accidentModel = mongoose.model('accident', accidentSchema, 'accident');
+
+module.exports = accidentModel;
+
+
+
